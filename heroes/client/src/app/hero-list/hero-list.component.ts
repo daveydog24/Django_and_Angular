3bad1d3b95b3ed2c708d9c356f881464f05921c1
David Wukelic <david.wukelic@gmail.com>
2019-01-29 15:49:54 -0800

    cleaned up hero-list component

--- heroes/client/src/app/hero-list/hero-list.component.ts
+++ heroes/client/src/app/hero-list/hero-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../heroes.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-hero-list',
@@ -9,42 +8,32 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class HeroListComponent implements OnInit {
-
     hero = {'hero': '', 'abilities': "none"};
     heroes = [];
 
-    constructor(private _heroesService: HeroesService, private _http: HttpClient) {
-        this._http = _http;
+    constructor(private _heroesService: HeroesService) {
         this._heroesService = _heroesService;
     }
     
-    ngOnInit() {
-    }  
+    ngOnInit() {}
 
     // access our hero service to get all the heroes in the service and eventually from the database.
     getAll(event){
         this._heroesService.getAll(data => {
             this.heroes = data;
-            console.log("heroes: ", this.heroes)
         });
     }
     
+    // sends updated data from ngModel to the hero service/database and updates our heroes
     createNewHero(){
-        // storing our recent data sent from ngModel after submitting the form into data
-        let data = this.hero
-        console.log(data)
-
-        // creates hero object that we are going to send to the service and eventually the database.
         let newHero = {
-            'hero': data.hero,
-            'abilities': data.abilities
+            'hero': this.hero.hero,
+            'abilities': this.hero.abilities
         };
 
-        // sends our new hero to our hero service and gets the new set of heros from the callback
-        // from the heroservice create Hero function.
+        // sends our new hero to hero service and gets the new set of heros from the callback
         this._heroesService.createHero(newHero, heroes => {
             this.heroes = heroes;
         });
-        // return this._http.post('heroes', this.heroes)
     }
 }
