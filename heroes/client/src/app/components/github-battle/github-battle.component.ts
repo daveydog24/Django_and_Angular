577ac62d80639c8322f5a014bf41cf7bb41ccd7d
David Wukelic <david.wukelic@gmail.com>
2019-02-11 13:05:29 -0800

    added sign in validation on github battle page

--- heroes/client/src/app/components/github-battle/github-battle.component.ts
+++ heroes/client/src/app/components/github-battle/github-battle.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { SignInService } from '../../services/sign-in.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-github-battle',
-  templateUrl: './github-battle.component.html',
-  styleUrls: ['./github-battle.component.css']
+    selector: 'app-github-battle',
+    templateUrl: './github-battle.component.html',
+    styleUrls: ['./github-battle.component.css']
 })
 export class GithubBattleComponent implements OnInit {
+    buttonON = false;
+    constructor(
+        private _signInService: SignInService,
+        private _router: Router
+    ){}
 
-  constructor() { }
+    ngOnInit() {
+        this._signInService.retrieveLoggedInUser(callback => {
+            if (callback != undefined) {
+                this.buttonON = true;
+            }
+            else {
+                this.buttonON = false;
+                this._router.navigate(['/home']);            
+            }
+        })
+    }
 
-  ngOnInit() {
-  }
+
 
 }
