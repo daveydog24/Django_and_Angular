cdc97ccbbdf3e4a4314ae93f303888c06b127a4d
David Wukelic <david.wukelic@gmail.com>
2019-04-22 22:32:30 -0700

    switched component data around and aded a service

--- compiler-playground/src/app/app.component.ts
+++ compiler-playground/src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'compiler-playground';
-  h1Style: boolean = false;
-
-  firstClick() {
-    this.h1Style = !this.h1Style;
-  }
 }
